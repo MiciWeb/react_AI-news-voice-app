ac288d46ea7aa026cfea7d1bcc17d3a26a2039d5
Mici Srsr <micipsa.sersour@epitech.eu>
2021-09-05 23:36:56 +0200

    fix env file bug

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { useEffect } from 'react'
 import alanBtn from "@alan-ai/alan-sdk-web"
 
-const alanKey = process.env.REACT_APP_API_KEY
+const alanKey = process.env.REACT_APP_ALAN_API_KEY
+const newsKey = process.env.REACT_APP_NEWS_API_KEY
 
 const App = () => {
 
     useEffect(() => {
         alanBtn({
             key: alanKey,
-            onCommand: ({ command }) => {
-                if (command === "testCommand") {
-                    alert("this code was executed")
+            onCommand: ({ command, articles }) => {
+                if (command === "newHeadlines") {
+                    console.log(articles)
                 }  
             }
         })
